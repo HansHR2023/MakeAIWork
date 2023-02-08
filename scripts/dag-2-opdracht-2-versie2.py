9fbaa81c57f092b59127596ca4314988e8541999
HansHR2023 <1072318@hr.nl>
2023-02-08 13:05:54 +0100

    updat script

--- scripts/dag-2-opdracht-2-versie2.py
+++ scripts/dag-2-opdracht-2-versie2.py
@@ -5,3 +5,9 @@ for i in word3:
     if i.isalpha():
         word3.replace(i," " )
 print( 'x',word3,'x')
+
+
+import re
+word5 =  "Pinda1-Kaas2-3"
+nwword = re.sub(r"[^0-9],' ', word5")
+print(nwword)

7ee66d80342694f9c82d746c13dfa85828dc0898
HansHR2023 <1072318@hr.nl>
2023-02-10 16:28:37 +0100

    Added regex

--- scripts/dag-2-opdracht-2-versie2.py
+++ scripts/dag-2-opdracht-2-versie2.py
@@ -1,13 +1,18 @@
 
 
-word3 = "Pinda1-Kaas2-3"
-for i in word3:
-    if i.isalpha():
-        word3.replace(i," " )
-print( 'x',word3,'x')
+# word3 = "Pinda1-Kaas2-3"
+# for i in word3:
+#     if i.isalpha():
+#         word3.replace(i," " )
+# print( 'x',word3,'x')
 
 
 import re
 word5 =  "Pinda1-Kaas2-3"
 nwword = re.sub(r"[^0-9],' ', word5")
 print(nwword)
+
+
+[16:26] Ji Yoon Kim (1069519)
+string = "happy 2023"new_str = re.sub(r"[^0-9]", " ", string)print(new_str)
+
